630247640d511b945ccb557fdfdc0d5b0d230e2b
Harshal Sheth <hsheth2@gmail.com>
2019-10-10 18:14:40 -0400

    Add body-parser

--- backend/server.js
+++ backend/server.js
@@ -6,8 +6,33 @@ const port = 3001;
 const morgan = require('morgan');
 app.use(morgan('tiny'));
 
+// Parse incoming requests.
+const bodyParser = require('body-parser');
+app.use(bodyParser.json());
+
 app.get('/api/ping', (req, res) => {
     res.send('pong');
 });
+
+let items = [
+    {
+        id: 0,
+        text: "test item",
+        completed: false,
+    },
+    {
+        id: 1,
+        text: "test item 2",
+        completed: false,
+    },
+];
+
+app.get('/api/items', (req, res) => {
+    res.json(items);
+})
+
+app.post('/api/items', (req, res) => {
+    // TODO: Setup this.
+})
 
 app.listen(port, () => console.log(`Server listening on port ${port}!`));
